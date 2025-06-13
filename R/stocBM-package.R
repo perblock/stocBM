fceb2d26d9ef75268fd2d58b4728e6f5c8c0ed81
perblock <perblock@gmx.de>
2025-06-13 10:31:54 +0200

    update metadata etc.

---
+++ R/stocBM-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats AIC coef
+#' @importFrom stats glm
+#' @importFrom stats heatmap
+#' @importFrom stats poisson
+#' @importFrom stats runif
+## usethis namespace: end
+NULL
